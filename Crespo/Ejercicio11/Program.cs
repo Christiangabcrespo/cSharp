fffa5b722e4923a1f4829439e7e0d69a6e377a31
Christiangabcrespo <christiangcrespo2295@gmail.com>
2022-11-17 17:47:18 -0300

    avances en 10, 11 y 12

--- Crespo/Ejercicio11/Program.cs
+++ Crespo/Ejercicio11/Program.cs
@@ -6,8 +6,54 @@ using System.Threading.Tasks;
 
 namespace Ejercicio11
 {
+
     class Program
     {
+        interface constantes
+        {
+            void EntrarPorra();
+            void Ganadores();
+            void generarResultados();
+        }
+
+        public class jugador 
+        {
+            public int dineroPorra = 1;
+            public int dineroIni = 5;
+            public int dineroRecaudado = 0;
+
+            public string nombre;
+            public int dinero;
+            public int porrasGanadas;
+            public string equipo;
+            public string[] resultados;
+            public string GanoPerdio = "";
+
+            public jugador(string nombre)
+            {
+                this.nombre = nombre;
+                this.dinero = dineroIni;
+                this.porrasGanadas = 0;
+            }
+
+            public void reiniciarResultados()
+            {
+                dineroRecaudado = 0;
+            }
+
+            public bool tieneDinero()
+            {
+                if (this.dinero >= dineroPorra)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+        }
         static void Main(string[] args)
         {
         }
--- Crespo/Ejercicio12/Program.cs
+++ Crespo/Ejercicio12/Program.cs
@@ -8,6 +8,71 @@ namespace Ejercicio12
 {
     class Program
     {
+        public class Metodos
+        {
+            public static Random random = new Random();
+
+            public static int generaNumeroAleatorio(int minimo, int maximo)
+            {
+                int num = random.Next(minimo, maximo);
+                return num;
+            }
+        }
+
+        public class Revolver
+        {
+            private int posicionBalaActual;
+            private int posicionBala;
+
+            public Revolver()
+            {
+                posicionBalaActual = Metodos.generaNumeroAleatorio(1, 6);
+                posicionBala = Metodos.generaNumeroAleatorio(1, 6);
+            }
+
+            public bool disparar()
+            {
+                bool exito = false;
+
+                if (posicionBalaActual == posicionBala)
+                {
+                    exito = true;
+                }
+                tambor();
+
+                return exito;
+
+            }
+
+            public void tambor()
+            {
+                if (posicionBalaActual == 6)
+                {
+                    posicionBalaActual = 1;
+                }
+                else
+                {
+                    posicionBalaActual++;
+                }
+            }
+        }
+
+        public class Jugador
+        {
+
+            private int id;
+            private string nombre;
+            private bool vivo;
+
+            public Jugador(int id)
+            {
+                this.id = id;
+                this.nombre = "Jugador " + id;
+                this.vivo = true;
+            }
+        }
+    
+       
         static void Main(string[] args)
         {
         }
