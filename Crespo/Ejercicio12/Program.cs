25513f4f36157d1fb89c024299012ea8ff1826f9
Christiangabcrespo <christiangcrespo2295@gmail.com>
2022-11-17 23:39:06 -0300

    Ejercico12

--- Crespo/Ejercicio12/Program.cs
+++ Crespo/Ejercicio12/Program.cs
@@ -70,11 +70,110 @@ namespace Ejercicio12
                 this.nombre = "Jugador " + id;
                 this.vivo = true;
             }
+            public void disparar(Revolver r)
+            {
+                Console.WriteLine(nombre + " se está apuntando");
+                if (r.disparar())
+                {
+                    this.vivo = false;
+                    Console.WriteLine(nombre + " murió");
+                }
+                else
+                {
+                    Console.WriteLine(nombre + " salvado");
+                }
+            }
+
+            public bool isVivo()
+            {
+                return vivo;
+            }
         }
-    
-       
+
+        public class Juego
+        {
+            private Jugador[] jugadores;
+            private Revolver revolver;
+
+            public Juego(int numJugadores)
+            {
+                jugadores = new Jugador[comprobarJugadores(numJugadores)];
+                crearJugadores();
+                revolver = new Revolver();
+            }
+
+            private int comprobarJugadores(int numJugadores)
+            {
+                Console.Write("Ingrese número de jugadores: ");
+                int cantJugadores = Int32.Parse(Console.ReadLine());
+                numJugadores = cantJugadores;
+
+                if (!(numJugadores >= 1 && numJugadores <= 6))
+                {
+                    numJugadores = 6;
+                }
+                return numJugadores;
+            }
+
+            private void crearJugadores()
+            {
+                for (int i = 0; i < jugadores.Length; i++)
+                {
+                    jugadores[i] = new Jugador(i + 1);
+                }
+            }
+
+            public bool finJuego()
+            {
+                for (int i = 0; i < jugadores.Length; i++)
+                {
+                    if (!jugadores[i].isVivo())
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+            public void ronda()
+            {
+                for (int i = 0; i < jugadores.Length; i++)
+                {
+                    jugadores[i].disparar(revolver);
+                }
+            }
+
+            public void rondaV2()
+            {
+                for (int i = 0; i < jugadores.Length; i++)
+                {
+                    if (jugadores[i].isVivo())
+                    {
+                        jugadores[i].disparar(revolver);
+                    }
+                }
+                return;
+            }
+        }
+
         static void Main(string[] args)
         {
+            Juego juego = new Juego(2);
+
+            while (!juego.finJuego())
+            {
+                Console.WriteLine("");
+                Console.WriteLine("Ronda 1: ");
+                juego.ronda();
+                Console.WriteLine("Ronda 2: ");
+                juego.rondaV2();
+                Console.WriteLine("Ronda 3: ");
+                juego.rondaV2();
+            }
+
+            Console.WriteLine("");
+            Console.WriteLine("Fin del juego.");
+            Console.ReadLine();
         }
     }
 }
